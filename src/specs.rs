bed49a74f6957cccc5a15e17780f9715d699e2d3
Zhengyao Lin <zhengyao-lin@outlook.com>
2024-10-09 20:35:00 -0400

    Add SpecTrieHard::get

--- src/specs.rs
+++ src/specs.rs
@@ -66,12 +66,34 @@ impl<T> SpecTrie<T> {
 }
 
 impl<T> SpecTrieHard<T> {
-    pub open spec fn wf(self) -> bool {
-        // Acyclicity (children's index is greater than parent's)
+    /// Acyclicity (children's index is greater than parent's)
+    pub open spec fn wf_acyclic(self) -> bool {
         &&& forall |i|
                 0 <= i < self.nodes.len() ==>
                 (#[trigger] self.nodes[i] matches SpecTrieState::Search(_, children) ==>
                 forall |j| #![trigger children[j].idx] 0 <= j < children.len() ==> i < children[j].idx < self.nodes.len())
+    }
+
+    /// SpecItem.key stored in some node should match the labels from root to that node
+    pub open spec fn wf_prefix(self, prefix: Seq<u8>, i: int) -> bool
+        decreases self.nodes.len() - i
+        when self.wf_acyclic() && 0 <= i < self.nodes.len()
+    {
+        match self.nodes[i] {
+            SpecTrieState::Leaf(item) => item.key == prefix,
+            SpecTrieState::Search(item, children) => {
+                &&& item matches Some(item) ==> item.key == prefix
+                &&& forall |j| #![trigger children[j]]
+                    0 <= j < children.len() ==>
+                    // Append one byte to the prefix
+                    self.wf_prefix(prefix + seq![children[j].prefix], children[j].idx)
+            }
+        }
+    }
+
+    pub open spec fn wf(self) -> bool {
+        &&& self.wf_acyclic()
+        &&& self.wf_prefix(seq![], 0)
     }
 
     /// Convert the subtree rooted at i to a SpecTrie
@@ -97,6 +119,65 @@ impl<T> SpecTrieHard<T> {
                 )
             }
         }
+    }
+
+    /// Check if there is a child with the given prefix
+    /// i.e. SearchNote::evaluate
+    pub open spec fn find_children(self, prefix: u8, children: Seq<SpecChildRef>) -> Option<int>
+        decreases children.len()
+    {
+        if children.len() <= 0 {
+            None
+        } else {
+            if children[0].prefix == prefix {
+                Some(children[0].idx)
+            } else {
+                self.find_children(prefix, children.drop_first())
+            }
+        }
+    }
+    
+    /// Search from the subtree at i
+    pub open spec fn get_helper(self, key: Seq<u8>, depth: int, i: int) -> Option<T>
+        decreases self.nodes.len() - i
+        when self.wf() && depth >= 0 && 0 <= i < self.nodes.len()
+    {
+        if depth > key.len() {
+            None
+        } else {
+            match self.nodes[i] {
+                SpecTrieState::Leaf(item) => {
+                    if item.key == key {
+                        Some(item.value)
+                    } else {
+                        None
+                    }
+                }
+                SpecTrieState::Search(item, children) => {
+                    if item.is_some() && item.unwrap().key == key {
+                        Some(item.unwrap().value)
+                    } else {
+                        // Check if there's any children with prefix key[depth]
+                        if let Some(next) = self.find_children(key[depth], children) {
+                            if i < next < self.nodes.len() {
+                                self.get_helper(key, depth + 1, next)
+                            } else {
+                                // Should not be reachable if self.wf()
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// Search for an element with the given key
+    /// Spec version of get_from_byte
+    pub open spec fn get(self, key: Seq<u8>) -> Option<T> {
+        self.get_helper(key, 0, 0)
     }
 }
 
