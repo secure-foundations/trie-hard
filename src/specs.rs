37f733b9825880af354fa8a7a2a40acb1f04f4f5
Zhengyao Lin <zhengyao-lin@outlook.com>
2024-10-12 17:58:58 -0400

    WIP lemma_wf_implies_unique_keys

--- src/specs.rs
+++ src/specs.rs
@@ -117,6 +117,45 @@ impl<T> SpecTrie<T> {
     // }
 }
 
+pub open spec fn is_prefix_of<S>(s1: Seq<S>, s2: Seq<S>) -> bool {
+    s2.len() >= s1.len() && s2.take(s1.len() as int) =~= s1
+}
+
+/// Return the first index which s1 and s2 differ, or min(s1.len(), s2.len())
+pub open spec fn diff_seq<S>(s1: Seq<S>, s2: Seq<S>) -> int
+decreases s1.len()
+{
+    if s1.len() == 0 || s2.len() == 0 || s1[0] != s2[0] {
+        0
+    } else {
+        1 + diff_seq(s1.drop_first(), s2.drop_first())
+    }
+}
+
+/// Different sequences must have the first different index
+/// or one is a prefix of another
+pub proof fn lemma_diff_seq<S>(s1: Seq<S>, s2: Seq<S>)
+    ensures ({
+        let diff = diff_seq(s1, s2);
+
+        &&& 0 <= diff <= s1.len()
+        &&& 0 <= diff <= s2.len()
+        &&& s1.take(diff) =~= s2.take(diff)
+        &&& diff < s1.len() && diff < s2.len() ==> s1[diff] != s2[diff]
+        // &&& s1.len() == s2.len() && s1 != s2 ==> diff < s1.len()
+    })
+
+    decreases s1.len()
+{
+    if s1.len() != 0 && s2.len() != 0 && s1[0] == s2[0] {
+        lemma_diff_seq(s1.drop_first(), s2.drop_first());
+        
+        let diff = diff_seq(s1, s2);
+        assert(s1.take(diff) =~= seq![s1[0]] + s1.drop_first().take(diff - 1));
+        assert(s2.take(diff) =~= seq![s2[0]] + s2.drop_first().take(diff - 1));
+    }
+}
+
 impl<T> SpecTrieHard<T> {
     /// Acyclicity (children's index is greater than parent's)
     pub open spec fn wf_acyclic(self) -> bool {
@@ -132,7 +171,7 @@ impl<T> SpecTrieHard<T> {
         when self.wf_acyclic() && 0 <= i < self.nodes.len()
     {
         match self.nodes[i] {
-            SpecTrieState::Leaf(item) => item.key.len() >= prefix.len() && item.key.take(prefix.len() as int) == prefix,
+            SpecTrieState::Leaf(item) => is_prefix_of(prefix, item.key),
             SpecTrieState::Search(item, children) => {
                 &&& item matches Some(item) ==> item.key == prefix
                 &&& forall |j| #![trigger children[j]]
@@ -181,18 +220,29 @@ impl<T> SpecTrieHard<T> {
         &&& path.len() > 0
         &&& path[0] == i
         &&& path.last() == j
-        &&& forall |k| 0 <= k < path.len() - 1 ==>
-            self.is_parent_of(#[trigger] path[k], path[k + 1])
-    }
-
-    /// Check if node j is a child of i
-    pub open spec fn is_parent_of(self, i: int, j: int) -> bool
-    {
         &&& 0 <= i < self.nodes.len()
         &&& 0 <= j < self.nodes.len()
-        &&& match self.nodes[i] {
-            SpecTrieState::Search(_, children) => exists |k| 0 <= k < children.len() && (#[trigger] children[k]).idx == j,
-            _ => false,
+        &&& forall |k| 0 <= k < path.len() - 1 ==>
+            self.is_parent_of(#[trigger] path[k], path[k + 1]).is_some()
+    }
+
+    /// Check if node j is a child of i, and returns the edge label
+    pub open spec fn is_parent_of(self, i: int, j: int) -> Option<u8>
+    {
+        if 0 <= i < self.nodes.len() && 0 <= j < self.nodes.len() {
+            match self.nodes[i] {
+                SpecTrieState::Search(_, children) => {
+                    if exists |k| 0 <= k < children.len() && (#[trigger] children[k]).idx == j {
+                        let k = choose |k| 0 <= k < children.len() && (#[trigger] children[k]).idx == j;
+                        Some(children[k].prefix)
+                    } else {
+                        None
+                    }
+                },
+                _ => None,
+            }
+        } else {
+            None
         }
     }
 
@@ -416,6 +466,222 @@ impl<T> SpecTrieHard<T> {
         }
     }
 
+    /// Get the concat of edge labels along a path
+    pub open spec fn get_prefix_for_path(self, path: Seq<int>) -> Seq<u8>
+        decreases path.len()
+        when self.is_path(path, path.first(), path.last())
+    {
+        if path.len() == 1 {
+            seq![]
+        } else {
+            self.get_prefix_for_path(path.drop_last()) +
+            seq![self.is_parent_of(path[path.len() - 2], path.last()).unwrap()]
+        }
+    }
+
+    // /// For any j reachable from i, if j has a key, then get_prefix_for_path(path) is a prefix of j
+    // pub proof fn lemma_path_prefix_is_key_helper(self, path1: Seq<int>, i: int, path2: Seq<int>, j: int)
+    //     requires
+    //         self.wf(),
+    //         self.is_path(path1, 0, i),
+    //         self.is_path(path2, i, j),
+
+    //     ensures
+    //         self.wf_prefix(self.get_prefix_for_path(path1), i),
+    //         // self.get_item(j) matches Some(item) ==> Self::is_prefix_of(self.get_prefix_for_path(path1), item.key),
+
+    //     decreases path1.len()
+    // {
+    //     if path1.len() > 1 {
+    //         let snd_last = path1[path1.len() - 2];
+    //         self.lemma_path_prefix_is_key_helper(path1.drop_last(), snd_last, seq![snd_last] + path2, j);
+
+    //         admit();
+
+    //         // // Label from snd_last to i
+    //         // let label = self.is_parent_of(snd_last, i).unwrap();
+
+    //         // if i != j {
+    //         //     if let Some(item) = self.get_item(j) {
+    //         //         let snd_last_node = self.nodes[snd_last];
+    //         //         let snd_last_children = snd_last_node->Search_1;
+
+    //         //         assert(Self::is_prefix_of(self.get_prefix_for_path(path1.drop_last()), item.key));
+    //         //         assert(self.wf_prefix(self.get_prefix_for_path(path1.drop_last()), snd_last));
+    //         //         // &&& item matches Some(item) ==> item.key == prefix
+    //         //         // &&& forall |j| #![trigger children[j]]
+    //         //         //     0 <= j < children.len() ==>
+    //         //         //     // Append one byte to the prefix
+    //         //         //     self.wf_prefix(prefix + seq![children[j].prefix], children[j].idx)
+                    
+    //         //         assert(exists |i_idx| 0 <= i_idx < snd_last_children.len() && (#[trigger] snd_last_children[i_idx]).idx == i);
+                    
+    //         //         // Index of i in the children of j
+    //         //         let i_idx = choose |i_idx| 0 <= i_idx < snd_last_children.len() && (#[trigger] snd_last_children[i_idx]).idx == i;
+
+    //         //         assert(self.get_prefix_for_path(path1) == self.get_prefix_for_path(path1.drop_last()) + seq![label]);
+    //         //         assert(self.wf_prefix(self.get_prefix_for_path(path1), i));
+                    
+    //         //         admit();
+    //         //     }
+    //         // }
+    //     }
+    // }
+
+    // /// For any node i, if path is a path from root to i,
+    // /// then get_prefix_for_path(path) is the same as the key at i
+    // /// if the key exists
+    // pub proof fn lemma_path_prefix_is_key(self, path: Seq<int>, i: int)
+    //     requires self.wf() && self.is_path(path, 0, i)
+    //     ensures
+    //         self.wf_prefix(self.get_prefix_for_path(path), i)
+        
+    //     decreases path.len()
+    //         // self.get_item(i) matches Some(item) ==> Self::is_prefix_of(self.get_prefix_for_path(path), item.key)
+    // {
+    //     // self.lemma_path_prefix_is_key_helper(path, i, seq![i], i);
+    //     if path.len() > 1 {
+    //         let snd_last = path[path.len() - 2];
+    //         self.lemma_path_prefix_is_key(path.drop_last(), snd_last);
+    //     }
+    // }
+
+    pub proof fn lemma_get_prefix_len(self, path: Seq<int>)
+        requires
+            self.wf(),
+            self.is_path(path, path.first(), path.last()),
+
+        ensures
+            self.get_prefix_for_path(path).len() == path.len() - 1,
+        
+        decreases path.len()
+    {
+        if path.len() > 1 {
+            self.lemma_get_prefix_len(path.drop_last());
+        }
+    }
+
+    pub proof fn lemma_get_prefix_alt(self, path: Seq<int>, i: int)
+        requires
+            self.wf(),
+            self.is_path(path, path.first(), path.last()),
+            0 <= i < path.len() - 1,
+
+        ensures
+            self.get_prefix_for_path(path)[i] == self.is_parent_of(path[i], path[i + 1]).unwrap(),
+    
+        decreases path.len()
+    {
+        self.lemma_get_prefix_len(path);
+
+        if i < path.len() - 2 {
+            self.lemma_get_prefix_alt(path.drop_last(), i);
+        }
+    }
+
+    pub proof fn lemma_path_to_wf_prefix(self, path: Seq<int>, i: int)
+        requires self.wf() && self.is_path(path, 0, i)
+        ensures
+            self.wf_prefix(self.get_prefix_for_path(path), i)
+        
+        decreases path.len()
+    {
+        if path.len() > 1 {
+            let snd_last = path[path.len() - 2];
+            self.lemma_path_to_wf_prefix(path.drop_last(), snd_last);
+        }
+    }
+
+    pub proof fn lemma_diff_path_prefix(self, path1: Seq<int>, path2: Seq<int>)
+        requires
+            self.wf(),
+            self.is_path(path1, 0, path1.last()),
+            self.is_path(path2, 0, path2.last()),
+            path1 != path2,
+
+        ensures
+            self.get_prefix_for_path(path1) != self.get_prefix_for_path(path2)
+    {
+        let diff = diff_seq(path1, path2);
+        lemma_diff_seq(path1, path2);
+
+        self.lemma_get_prefix_len(path1);
+        self.lemma_get_prefix_len(path2);
+
+        // if diff < path1.len() && diff < path2.len() {
+        //     admit();
+        // }
+        // self.lemma_get_prefix_alt(path1, diff - 1);
+        // self.lemma_get_prefix_alt(path2, diff - 1);
+
+        admit();
+    }
+
+    /// Similar as get_helper, but instead return the path (from i) to the node with the given key
+    pub open spec fn get_helper_with_path(self, key: Seq<u8>, depth: int, i: int) -> Option<Seq<int>>
+        decreases self.nodes.len() - i
+        when self.wf() && 0 <= depth <= key.len() && 0 <= i < self.nodes.len()
+    {
+        match self.nodes[i] {
+            SpecTrieState::Leaf(item) => {
+                if item.key == key {
+                    Some(seq![i])
+                } else {
+                    None
+                }
+            }
+            SpecTrieState::Search(item, children) => {
+                if key.len() == depth {
+                    match item {
+                        Some(..) => Some(seq![i]),
+                        None => None,
+                    }
+                } else {
+                    // Check if there's any children with prefix key[depth]
+                    if let Some(next) = Self::find_children(key[depth], children) {
+                        if i < next < self.nodes.len() {
+                            match self.get_helper_with_path(key, depth + 1, next) {
+                                Some(path) => Some(seq![i] + path),
+                                None => None,
+                            }
+                        } else {
+                            // Should not be reachable if self.wf()
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                }
+            }
+        }
+    }
+
+    /// If get_helper succeeds, we have a path from i to the found node
+    pub proof fn lemma_get_helper_implies_path(self, key: Seq<u8>, depth: int, i: int)
+        requires
+            self.wf(),
+            0 <= depth <= key.len(),
+            0 <= i < self.nodes.len(),
+
+        ensures
+            self.get_helper(key, depth, i) matches Some(value) ==>
+                exists |j: int| {
+                    &&& #[trigger] self.get_item(j) matches Some(item)
+                    &&& item.key == key
+                    &&& item.value == value
+                    &&& exists |path: Seq<int>| self.is_path(path, i, j)
+                },
+
+            self.get_helper(key, depth, i).is_none() ==>
+                forall |j: int| #![trigger self.nodes[j]] i <= j < self.nodes.len() ==> {
+                    ||| self.get_item(j) matches Some(item) && item.key != key
+                    ||| self.get_item(j).is_none()
+                    ||| forall |path: Seq<int>| !#[trigger] self.is_path(path, i, j)
+                }
+    {
+        admit();
+    }
+
     /// Prove that for a well-formed SpecTrieHard, keys stored in nodes are unique
     pub proof fn lemma_wf_implies_unique_keys(self)
         requires self.wf()
@@ -443,32 +709,56 @@ impl<T> SpecTrieHard<T> {
             let path_i = choose |ancestors: Seq<int>| self.is_path(ancestors, 0, i);
             let path_j = choose |ancestors: Seq<int>| self.is_path(ancestors, 0, j);
         
-            // Find the first diverging position of these two paths (which must exists since i != j)
+            // path_i must be different from path_j, since i != j
+            self.lemma_path_to_wf_prefix(path_i, i);
+            self.lemma_path_to_wf_prefix(path_j, j);
 
-            admit();
-        }
-    }
+            // let path_i_prefix = self.get_prefix_for_path(path_i);
+            // let path_j_prefix = self.get_prefix_for_path(path_j);
 
-    pub proof fn lemma_get_to_exists_key_helper(self, key: Seq<u8>, depth: int, j: int)
-        requires self.wf()
-        ensures
-            // TODO: Maybe add a witness function that directly computes i?
-            self.get_helper(key, depth, j) matches Some(value) ==>
-                exists |i| {
-                    &&& j <= i < self.nodes.len()
-                    &&& #[trigger] self.get_item(i) matches Some(item)
-                    &&& item.key == key
-                    &&& item.value == value
+            let path_i_prefix = self.get_prefix_for_path(path_i);
+            let path_j_prefix = self.get_prefix_for_path(path_j);
+
+            match (self.nodes[i], self.nodes[j]) {
+                // If both are leaf nodes, then there it's not possible that
+                // path_i is a prefix or path_j or vice versa.
+                // So there must be a index where two paths differ, and that
+                // causes the prefix to differ
+                (SpecTrieState::Leaf(item1), SpecTrieState::Leaf(item2)) => {
+                    assert(!is_prefix_of(path_i, path_j));
+                    assert(!is_prefix_of(path_j, path_i));
+
+                    // Exists an index where the two paths differ
+                    let diff = diff_seq(path_i, path_j);
+                    lemma_diff_seq(path_i, path_j);
+                    
+                    assert(path_i.take(diff) =~= path_j.take(diff));
+                    assert(path_i[diff] != path_j[diff]);
+                    assert(diff >= 1);
+
+                    assert(path_i[diff - 1] == path_i.take(diff)[diff - 1]);
+                    assert(path_j[diff - 1] == path_j.take(diff)[diff - 1]);
+                    assert(path_i[diff - 1] == path_j[diff - 1]);
+
+                    self.lemma_get_prefix_alt(path_i, diff - 1);
+                    self.lemma_get_prefix_alt(path_j, diff - 1);
+                    self.lemma_get_prefix_len(path_i);
+                    self.lemma_get_prefix_len(path_j);
+                    
+                    assert(path_i_prefix[diff - 1] != path_j_prefix[diff - 1]);
                 },
 
-            // TODO: Need to use wf_no_junk here
-            self.get_helper(key, depth, j).is_none() ==>
-                forall |i| j <= i < self.nodes.len() ==>
-                    (#[trigger] self.get_item(i) matches Some(item) ==>
-                        item.key != key),
-        decreases self.nodes.len() - j
-    {
-        admit();
+                // If both are search nodes, then their keys are equal to the path labels
+                // which are different. So their keys are different.
+                (SpecTrieState::Search(Some(item1), ..), SpecTrieState::Search(Some(item2), ..)) => {
+                    self.lemma_diff_path_prefix(path_i, path_j);
+                },
+
+                _ => {
+                    admit();
+                },
+            }
+        }
     }
 
     /// Prove that get(k) is some iff there is some node with key k, and the value is the same
@@ -489,7 +779,7 @@ impl<T> SpecTrieHard<T> {
                     (#[trigger] self.get_item(i) matches Some(item) ==>
                         item.key != key),
     {
-        self.lemma_get_to_exists_key_helper(key, 0, 0);
+        self.lemma_get_helper_implies_path(key, 0, 0);
     }
 
     /// Helper for lemma_get_alt_to_exists_key
